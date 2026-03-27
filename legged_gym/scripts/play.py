d79d68f38b8c74abb7629af2b42873c74d34ca42
Sichao Fu <f.sichao@outlook.com>
2026-03-27 19:38:42 +0800

    Fix a bug where seed argument was not being passed to the environment configuration.

--- legged_gym/scripts/play.py
+++ legged_gym/scripts/play.py
@@ -74,7 +74,7 @@ def play(args):
     
     # export policy as a jit module (used to run it from C++)
     if EXPORT_POLICY:
-        path = os.path.join(LEGGED_GYM_ROOT_DIR, 'logs', train_cfg.runner.experiment_name, 'exported', args.load_run + 'policies')
+        path = os.path.join(LEGGED_GYM_ROOT_DIR, 'logs', train_cfg.runner.experiment_name, 'exported', args.load_run + '_policies')
         export_policy_as_jit(ppo_runner.alg.actor_critic, path)
         print('Exported policy as jit script to: ', path)
 
